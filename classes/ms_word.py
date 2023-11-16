e412f226a5618589a7376aa204bef76ce14ad773
jjrboucher <jjrboucher@users.noreply.github.com>
2023-11-16 09:57:55 -0500

    Removed outside parenthesis on line 205, as it was causing a syntax error for some. Runs correctly without them, so they appear to be unnecessary.

--- classes/ms_word.py
+++ classes/ms_word.py
@@ -202,7 +202,7 @@ class Docx:
         """
         month = {1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr", 5: "May", 6: "Jun",
                  7: "Jul", 8: "Aug", 9: "Sep", 10: "Oct", 11: "Nov", 12: "Dec"}
-        with (zipfile.ZipFile(self.msword_file, 'r') as zip_file):
+        with zipfile.ZipFile(self.msword_file, 'r') as zip_file:
             # returns XML files in the DOCx
             xml_files = {}
             for file_info in zip_file.infolist():
